fbaa6de1aed5f4e15439ca7c7c5b00a35fc3efc6
Jason Ernst <jason@left.io>
2017-11-02 08:14:06 -0700

    updated install to create the config.php file

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-cp config.php.example config.php
+#cp config.php.example config.php
 
+echo "Enter the DB_SERVER: "
+read server
+
+echo "Enter the DB_USER: "
+read user
+
+echo "Enter the DB_PASSWORD: "
+read password
+
+echo "Writing to config file: config.php"
+echo '<?php \n
+define("DB_NAME", "bw_monitor");\n
+define("DB_SERVER", "$server");\n
+define("DB_USER", "$user");\n
+define("DB_PASSWORD", "$password");\n
+?>' > config.php;
